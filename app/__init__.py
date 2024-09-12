f38e03a0fd9beed2b3afa83e92fb786bcdf08cce
hsynalv <hhsynalv@gmail.com>
2024-09-12 20:54:18 +0300

    Production ve Development Config

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,37 @@
 from flask import Flask
 from flask_cors import CORS
 from pymongo import MongoClient
-
-from .config import Config
+import os
 from .auth import configure_oauth
 from .routes.text_to_image_routes import text_to_image_bp
+from dotenv import load_dotenv
+from flask_wtf.csrf import CSRFProtect
+
+# Ortam değişkenine göre doğru .env dosyasını yükle
+env_file = ".env.development" if os.getenv('FLASK_ENV') == 'development' else ".env.production"
+load_dotenv(env_file)  # Doğru .env dosyasını yükle
+
+# Ortam yapılandırma dosyalarını içe aktar
+from app.config.development import DevelopmentConfig
+from app.config.production import ProductionConfig
 def create_app():
+
     app = Flask(__name__)
-    CORS(app, supports_credentials=True)
-    app.config.from_object(Config)
+
+    csrf = CSRFProtect()
+    csrf.init_app(app)
+
+    # Ortam değişkenine göre yapılandırmayı yükle
+    if os.getenv('FLASK_ENV') == 'production':
+        app.config.from_object(ProductionConfig)
+    else:
+        app.config.from_object(DevelopmentConfig)
+
+    # CORS ayarları (Üretim ve Geliştirme için farklı olabilir)
+    if app.config.get('ENV', 'development') == 'production':
+        CORS(app, resources={r"/api/*": {"origins": "https://your-production-site.com"}})
+    else:
+        CORS(app, supports_credentials=True)
 
     # MongoDB bağlantısı
     client = MongoClient(app.config["MONGO_URI"])
---
+++ app/config/development.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class DevelopmentConfig:
+    DEBUG = True
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    MONGO_URI = os.getenv("MONGO_URI")
+    DISCORD_CLIENT_ID = os.getenv("DISCORD_CLIENT_ID")
+    DISCORD_CLIENT_SECRET = os.getenv("DISCORD_CLIENT_SECRET")
+    DISCORD_REDIRECT_URI = os.getenv("DISCORD_REDIRECT_URI")
+    GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+    GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+    GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI")
+    SECRET_KEY = os.getenv("SECRET_KEY", "horiar-bir-web3-platformu")
+    RUNPOD_URL = os.getenv("RUNPOD_URL")
+    RUNPOD_API_KEY = os.getenv("RUNPOD_API_KEY")
+
