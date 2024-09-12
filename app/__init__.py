1823ee2bc1e7139603380ea9db615c7f29df3de1
hsynalv <hhsynalv@gmail.com>
2024-09-13 01:23:57 +0300

    logging has been added

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,15 @@
+import os
 from flask import Flask
 from flask_cors import CORS
-from pymongo import MongoClient
 from mongoengine import connect
-import os
-from .auth import configure_oauth
-
-from .routes.text_to_image_routes import text_to_image_bp
 from dotenv import load_dotenv
-from flask_wtf.csrf import CSRFProtect
+
+from .auth import configure_oauth
+from .routes.text_to_image_routes import text_to_image_bp
 
 # Extensions ve helper fonksiyonları içe aktar
 from app.extensions.errors import register_error_handlers
 from app.extensions.blueprints import register_blueprints
-
-from app.auth import configure_oauth
 
 # Ortam değişkenine göre doğru .env dosyasını yükle
 env_file = ".env.development" if os.getenv('FLASK_ENV') == 'development' else ".env.production"
@@ -22,9 +18,15 @@ load_dotenv(env_file)  # Doğru .env dosyasını yükle
 # Ortam yapılandırma dosyalarını içe aktar
 from app.config.development import DevelopmentConfig
 from app.config.production import ProductionConfig
+from app.extensions.logging_config import setup_logging
+from app.auth import configure_oauth
+
 def create_app():
 
     app = Flask(__name__)
+
+    # Loglama yapılandırmasını başlat
+    setup_logging()
 
     #csrf = CSRFProtect()
     #csrf.init_app(app)
--- app/extensions/errors.py
+++ app/extensions/errors.py
@@ -1,3 +1,4 @@
+import logging
 from flask import jsonify
 from app.errors.unauthorized_error import UnauthorizedError
 from app.errors.not_found_error import NotFoundError
@@ -6,21 +7,29 @@ from app.errors.validation_error import ValidationError
 def register_error_handlers(app):
     @app.errorhandler(UnauthorizedError)
     def handle_unauthorized_error(e):
+        # UnauthorizedError için loglama (WARNING seviyesi)
+        logging.warning(f"Unauthorized access attempt: {e.message}")
         return jsonify(e.to_dict()), e.status_code
 
     @app.errorhandler(NotFoundError)
     def handle_not_found_error(e):
+        # NotFoundError için loglama (WARNING seviyesi)
+        logging.warning(f"Resource not found: {e.message}")
         return jsonify(e.to_dict()), e.status_code
 
     @app.errorhandler(ValidationError)
     def handle_validation_error(e):
+        # ValidationError için loglama (ERROR seviyesi)
+        logging.error(f"Validation error: {e.message}")
         return jsonify(e.to_dict()), e.status_code
 
     @app.errorhandler(Exception)
     def handle_global_exception(e):
+        # Genel hatalar için loglama (ERROR seviyesi)
         code = 500
         if hasattr(e, 'code'):
             code = e.code
+        logging.critical(f"Unhandled exception occurred: {str(e)}")
         return jsonify({
             "error": "Internal Server Error",
             "message": str(e),
