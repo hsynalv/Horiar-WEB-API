4a6783e073bd93be055bcdd0ff6fe663b7383bc0
hsynalv <hhsynalv@gmail.com>
2024-09-17 00:19:01 +0300

    update cors bug

--- app/routes/text_to_image_routes.py
+++ app/routes/text_to_image_routes.py
@@ -6,23 +6,11 @@ from ..auth import jwt_required
 
 text_to_image_bp = Blueprint('text_to_image_bp', __name__)
 
-@text_to_image_bp.route('/generate-image-direct', methods=['POST', 'OPTIONS'])
+@text_to_image_bp.route('/generate-image-direct', methods=['POST'])
 @jwt_required(pass_payload=True)
 @daily_request_limit
 @ban_check
 def generate_image_direct(payload):
-    if request.method == 'OPTIONS':
-        # Preflight OPTIONS isteğine 200 OK ile yanıt dönüyoruz
-        response = current_app.response_class(
-            status=200,
-            headers={
-                'Access-Control-Allow-Origin': 'https://www.horiar.com',
-                'Access-Control-Allow-Methods': 'POST, OPTIONS',
-                'Access-Control-Allow-Headers': 'Authorization, Content-Type',
-                'Access-Control-Allow-Credentials': 'true'
-            }
-        )
-        return response
     data = request.json
     prompt = data.get('prompt')
 
