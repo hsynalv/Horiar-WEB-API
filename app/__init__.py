f8ce07af009151db36a0ab77ccf69de1c421b8e8
hsynalv <hhsynalv@gmail.com>
2024-09-16 21:59:58 +0300

    update init file

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 from flask_admin import Admin
 from flask_admin.contrib.mongoengine import ModelView  # Flask-Admin için ModelView
 
-from .auth import configure_oauth
-from .routes.text_to_image_routes import text_to_image_bp
+from app.auth import configure_oauth
+from app.routes.text_to_image_routes import text_to_image_bp
 
 # Extensions ve helper fonksiyonları içe aktar
 from app.extensions.errors import register_error_handlers
@@ -23,39 +23,40 @@ load_dotenv(env_file)  # Doğru .env dosyasını yükle
 # Ortam yapılandırma dosyalarını içe aktar
 from app.config.development import DevelopmentConfig
 from app.config.production import ProductionConfig
-from app.auth import configure_oauth
-
 
 # MongoEngine örneği oluştur
 db = MongoEngine()
 
-def create_app():
 
+def create_app():
     app = Flask(__name__)
 
     # Loglama yapılandırmasını başlat
     setup_logging()
 
-    #csrf = CSRFProtect()
-    #csrf.init_app(app)
-
     # Ortam değişkenine göre yapılandırmayı yükle
-    if os.getenv('FLASK_ENV') == 'production':
+    if app.config['ENV'] == 'production':
         app.config.from_object(ProductionConfig)
     else:
         app.config.from_object(DevelopmentConfig)
 
-    # CORS ayarları (Üretim ve Geliştirme için farklı olabilir)
-    if app.config.get('ENV', 'development') == 'production':
-        CORS(app, resources={r"/*": {"origins": "https://www.horiar.com", "supports_credentials": True}})
+    print(f"Flask ENV: {app.config.get('ENV')}")
+
+    # CORS ayarları
+    if app.config['ENV'] == 'production':
+        CORS(app, resources={r"/*": {"origins": ["https://www.horiar.com"], "supports_credentials": True}})
     else:
         CORS(app, supports_credentials=True)
 
-
     # MongoDB bağlantı ayarları .env'den çekiliyor
+    mongo_uri = os.getenv('MONGO_URI')
+    if not mongo_uri:
+        raise ValueError("MONGO_URI not set in the environment variables.")
+
     app.config['MONGODB_SETTINGS'] = {
-        'host': os.getenv('MONGO_URI')  # .env.development dosyasındaki MONGO_URI değerini kullan
+        'host': mongo_uri
     }
+
     # MongoDB bağlantısını başlat
     db.init_app(app)
 
