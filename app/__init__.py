35a80967a5114161abb8e621cfae7fc6b71716c7
hsynalv <hhsynalv@gmail.com>
2024-09-19 19:27:07 +0300

    update init file CORS

--- app/__init__.py
+++ app/__init__.py
@@ -43,10 +43,7 @@ def create_app():
 
     # CORS ayarları
     if app.config['ENV'] == 'production':
-        CORS(app, resources={
-            r"/admin/*": {"origins": ["https://horiar.com", "https://api.horiar.com"], "supports_credentials": True},
-            r"/*": {"origins": ["https://horiar.com"], "supports_credentials": True}
-        })
+        CORS(app, resources={r"/*": {"origins": ["https://www.horiar.com"], "supports_credentials": True}})
     else:
         CORS(app, supports_credentials=True)
 
