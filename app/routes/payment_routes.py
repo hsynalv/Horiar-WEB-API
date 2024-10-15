c6d8d85c779a7df00293b1d2ae117c1612a14f70
hsynalv <hhsynalv@gmail.com>
2024-10-15 17:25:08 +0300

    callback-ok test v2

--- app/routes/payment_routes.py
+++ app/routes/payment_routes.py
@@ -32,12 +32,12 @@ def get_token(payload):
 @payment_bp.route('/callback-ok', methods=['POST'])
 @csrf.exempt  # CSRF korumasını devre dışı bırak
 def callback_ok():
-    print("callback ok girildi")
+    logging.info("callback ok girildi")
     # Sadece POST isteklerini kabul et
     if request.method != 'POST':
         return '', 400  # Bad Request
 
-    print("method post methodu")
+    logging.info("method post methodu")
 
     post = request.form
     # API Entegrasyon Bilgileri
@@ -47,7 +47,7 @@ def callback_ok():
     hash_str = post['merchant_oid'] + merchant_salt + post['status'] + post['total_amount']
     hash = base64.b64encode(hmac.new(merchant_key, hash_str.encode(), hashlib.sha256).digest()).decode()
 
-    print("hash çıkartıldı")
+    logging.info("hash çıkartıldı")
 
     # Oluşturulan hash'i, paytr'dan gelen post içindeki hash ile karşılaştır
     if hash != post['hash']:
@@ -58,12 +58,12 @@ def callback_ok():
     # Burada siparişi veritabanından sorgulayıp onaylayabilir veya iptal edebilirsiniz.
     if status == 'success':  # Ödeme Onaylandı
         # Siparişi onaylayın
-        print(f"Order {merchant_oid} has been approved.")
+        logging.info(f"Order {merchant_oid} has been approved.")
         # Müşteriye bildirim yapabilirsiniz (SMS, e-posta vb.)
         # Güncel tutarı post['total_amount'] değerinden alın.
     else:  # Ödemeye Onay Verilmedi
         # Siparişi iptal edin
-        print(f"Order {merchant_oid} has been canceled. Reason: {post.get('failed_reason_msg', 'Unknown reason')}")
+        logging.info(f"Order {merchant_oid} has been canceled. Reason: {post.get('failed_reason_msg', 'Unknown reason')}")
     # Bildirimin alındığını PayTR sistemine bildir.
     return 'OK', 200  # OK
 
