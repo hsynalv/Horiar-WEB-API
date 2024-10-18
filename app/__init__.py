b4fca89c8833330d7663332f4a6694441c87dfd5
hsynalv <hhsynalv@gmail.com>
2024-10-18 16:06:16 +0300

    updated cors policy

--- app/__init__.py
+++ app/__init__.py
@@ -47,7 +47,9 @@ def create_app():
 
     # CORS ayarları
     if app.config['ENV'] == 'production':
-        CORS(app, resources={r"/*": {"origins": ["https://www.horiar.com", "https://accounts.google.com", "https://discord.com"], "supports_credentials": True}})
+        CORS(app, resources={r"/*": {
+            "origins": ["https://www.horiar.com", "https://horiar.com", "https://accounts.google.com",
+                        "https://discord.com"], "supports_credentials": True}})
     else:
         CORS(app, supports_credentials=True)
 
