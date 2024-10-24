644256005ca45c6300537ebe53f5f64f103c7396
hsynalv <hhsynalv@gmail.com>
2024-10-24 12:47:53 +0300

    login google bug fix

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -66,7 +66,7 @@ def discord_callback():
 def login_google():
     google = oauth.create_client('google')
     redirect_uri = url_for('user_bp.google_callback', _external=True)
-    return google.authorize_redirect(redirect_uri)
+    return google.authorize_redirect(redirect_uri, prompt='select_account')
 
 
 @user_bp.route('/login/google/callback')
