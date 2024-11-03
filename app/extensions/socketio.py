b74e2fd78864a6f42a01209c3ceba9857a107e09
hsynalv <hhsynalv@gmail.com>
2024-11-03 21:35:03 +0300

    cors update and admin panel

--- app/extensions/socketio.py
+++ app/extensions/socketio.py
@@ -1,7 +1,18 @@
+from flask import Flask
 from flask_socketio import SocketIO
+from flask_cors import CORS
 
-socketio = SocketIO(cors_allowed_origins=["http://localhost", "http://127.0.0.1","http://127.0.0.1:5500", "https://horiar.com", "https://www.horiar.com"])
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": ["https://www.horiar.com", "https://horiar.com", "http://localhost:5000", "http://127.0.0.1:5500", "http://localhost:3000", "http://127.0.0.1:3000"], "supports_credentials": True}})
 
+socketio = SocketIO(app, cors_allowed_origins=[
+    "http://localhost",
+    "http://127.0.0.1",
+    "http://127.0.0.1:5500",
+    "http://127.0.0.1:3000",
+    "https://horiar.com",
+    "https://www.horiar.com"
+])
 
 def notify_user_via_websocket(user_id, message):
     """
@@ -10,5 +21,4 @@ def notify_user_via_websocket(user_id, message):
     :param user_id: Bildirimin gönderileceği kullanıcı kimliği
     :param message: Gönderilecek bildirim mesajı
     """
-    # WebSocket bağlantısı ile kullanıcıya mesaj gönderiyoruz
     socketio.emit('user_notification', {'user_id': user_id, 'message': message})
