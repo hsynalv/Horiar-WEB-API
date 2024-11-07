d7685da98678018344bfaae43510fdb1170ed445
hsynalv <hhsynalv@gmail.com>
2024-11-07 22:43:45 +0300

    emit room bug fixed

--- app/routes/websocket_routes.py
+++ app/routes/websocket_routes.py
@@ -4,31 +4,29 @@ from flask import request
 from flask_socketio import emit, join_room, rooms
 from app import socketio
 
+
 @socketio.on('connect')
 def handle_connect():
-    logging.info(f"Client connected: {request.sid}")
+    logging.error(f"Client connected: {request.sid}")
     emit('message', {'status': 'connected', 'message': 'Connection established'})
 
 
-@socketio.on('joinRoom')
+@socketio.on('join')
 def handle_join(data):
+    logging.error("join fonksiyonu tetiklendi.")
     room = data.get('room')
-    logging.info(f"join fonksiyonunda gelen data: {data}")
-    logging.info(f"gelen room = {room}")
+    logging.error(f"join fonksiyonunda gelen data: {data}")
+    logging.error(f"gelen room = {room}")
     if room:
         logging.info(f"Joining room {room}")
         join_room(room)
 
-        # Kullanıcının gerçekten katıldığını doğrulayalım
-        if room in rooms(request.sid):
-            logging.info(f"Kullanıcı {room} odasına başarıyla katıldı.")
-            emit('message', {'status': 'joined', 'room': room}, to=room)
-        else:
-            logging.warning(f"{room} odasına katılım başarısız.")
-            emit('message', {'status': 'error', 'message': f'Could not join room {room}'})
+        # Kullanıcının gerçekten katıldığını doğrulama
+        emit('message', {'status': 'joined', 'room': room}, to=room)
     else:
         emit('message', {'status': 'error', 'message': 'Room ID is required to join'})
 
+
 @socketio.on('disconnect')
 def handle_disconnect():
-    print(f"Client disconnected: {request.sid}")
+    logging.info(f"Client disconnected: {request.sid}")
