0e93f2aeb5f393d4d87f642d3a87afaf577234fe
hsynalv <hhsynalv@gmail.com>
2024-11-07 21:49:05 +0300

    emit room bug fixed v3

--- app/extensions/socketio.py
+++ app/extensions/socketio.py
@@ -2,14 +2,19 @@ from flask import Flask
 from flask_socketio import SocketIO
 from flask_cors import CORS
 
-socketio = SocketIO(cors_allowed_origins=[
-    "http://localhost",
-    "http://127.0.0.1",
-    "http://127.0.0.1:5500",
-    "http://127.0.0.1:3000",
-    "https://horiar.com",
-    "https://www.horiar.com"
-])
+socketio = SocketIO(
+    cors_allowed_origins=[
+        "http://localhost",
+        "http://127.0.0.1",
+        "http://127.0.0.1:5500",
+        "http://127.0.0.1:3000",
+        "https://horiar.com",
+        "https://www.horiar.com"
+    ],
+    async_mode='eventlet',  # Yalnızca WebSocket bağlantısı için 'eventlet' seçin
+    logger=True,
+    engineio_logger=True  # Bağlantı detaylarını loglamak için
+)
 
 
 def notify_user_via_websocket(user_id, message):
