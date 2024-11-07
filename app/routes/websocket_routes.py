059fcbe155cab33d4b755765638ce05e4cd4a36d
hsynalv <hhsynalv@gmail.com>
2024-11-07 22:27:53 +0300

    emit room bug fixed v3

--- app/routes/websocket_routes.py
+++ app/routes/websocket_routes.py
@@ -1,7 +1,7 @@
 import logging
 
 from flask import request
-from flask_socketio import emit, join_room
+from flask_socketio import emit, join_room, rooms
 from app import socketio
 
 @socketio.on('connect')
@@ -13,12 +13,17 @@ def handle_connect():
 @socketio.on('join')
 def handle_join(data):
     room = data.get('room')
-    logging.info(f"join fonksiyonunda gelen data: {data}")
-    logging.info(f"gelen room = {room}")
     if room:
-        logging.info(f"Joining room {room}")
         join_room(room)
-        emit('message', {'status': 'joined', 'room': room}, to=room)
+        # Katılımı loglamak
+        logging.info(f"Kullanıcı room'a katıldı: {room}")
+
+        # Kullanıcının room'a katılıp katılmadığını kontrol et
+        if room in rooms(request.sid):
+            logging.info(f"Room '{room}' başarılı şekilde katılındı.")
+            emit('message', {'status': 'joined', 'room': room}, to=room)
+        else:
+            logging.warning(f"Kullanıcı room'a katılamadı: {room}")
     else:
         emit('message', {'status': 'error', 'message': 'Room ID is required to join'})
 
