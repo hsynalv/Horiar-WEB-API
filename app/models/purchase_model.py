bbc898309938cc158d14428ebff0a04259c8aac4
hsynalv <hhsynalv@gmail.com>
2024-11-15 23:03:41 +0300

    added purchase model bug fix v2

--- app/models/purchase_model.py
+++ app/models/purchase_model.py
@@ -4,7 +4,7 @@ from app.models.user_model import User  # Kullanıcı modelini eklemek için uyg
 from app.models.package_model import Package  # Paket modelini eklemek için uygun yolu kullanın
 
 class Purchase(Document):
-    user = ReferenceField(User, required=True)  # Satın alımı yapan kullanıcı
+    username = ReferenceField(User, required=True)  # Satın alımı yapan kullanıcı
     package = ReferenceField(Package, required=True)  # Satın alınan paket
     amount = FloatField(required=True)  # Satın alım tutarı (gelir)
     payment_date = DateTimeField(default=datetime.utcnow)  # Satın alımın gerçekleştiği tarih
@@ -14,7 +14,7 @@ class Purchase(Document):
     def to_dict(self):
         return {
             "id": str(self.id),
-            "user": str(self.user.id) if self.user else None,
+            "username": str(self.user.id) if self.user else None,
             "package": str(self.package.id) if self.package else None,
             "amount": self.amount,
             "payment_date": self.payment_date.isoformat(),
