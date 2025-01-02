0de936f7e1b0ce5b60724d84176d26b397154ddf
hsynalv <hhsynalv@gmail.com>
2025-01-03 01:37:57 +0300

    mail system has been updated v2

--- app/utils/mail_utils.py
+++ app/utils/mail_utils.py
@@ -1,5 +1,9 @@
 from flask_mail import Message
 from app.extensions.mail import mail  # Doğru mail objesini import ettik
+import logging
+
+# Logger yapılandırması
+logger = logging.getLogger(__name__)
 
 def send_email(subject, recipients, body, html_body=None):
     msg = Message(
@@ -8,15 +12,25 @@ def send_email(subject, recipients, body, html_body=None):
         body=body,
         html=html_body
     )
-    mail.send(msg)
+    try:
+        mail.send(msg)
+        logger.info(f"Email sent successfully to {recipients}")
+    except Exception as e:
+        error_message = f"Failed to send email to {recipients}. Error: {str(e)}"
+        logger.error(error_message)
+        send_error_email("Email Send Error", error_message)
 
 
 def send_error_email(subject, error_details):
-    # E-posta içeriği
     msg = Message(
         subject=subject,
         recipients=["hhsynalv@gmail.com"],  # Hata e-postalarını alacak admin e-posta adresleri
         body=error_details
     )
-    mail.send(msg)
-    print("mail gönderildi")
+    try:
+        mail.send(msg)
+        logger.info("Error email sent successfully.")
+    except Exception as e:
+        logger.critical(f"Failed to send error email. Error: {str(e)}")
+        # print kullanımı loglarla değiştirilmiştir.
+
