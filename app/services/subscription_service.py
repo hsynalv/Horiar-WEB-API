1a8e06367fff5554ecbd3c6510de7ada9744c16b
hsynalv <hhsynalv@gmail.com>
2025-01-25 23:32:06 +0300

    subscription service has been updated

--- app/services/subscription_service.py
+++ app/services/subscription_service.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from app.models.subscription_model import Subscription
 from app.services.base_service import BaseService
 
@@ -11,7 +13,15 @@ class SubscriptionService(BaseService):
         Verilen user_id'ye göre bir subscription kaydını döndürür.
         """
         try:
-            subscription = Subscription.objects(user_id=user_id).first()
+            # Bugünün tarihini al
+            today = datetime.utcnow()
+
+            # Tarihi bugünden sonra olan kayıtları getir
+            subscription = Subscription.objects(
+                user_id=user_id,
+                subscription_end_date__gte=today  # Tarih bugünden büyük veya eşit olanlar
+            ).first()
+
             if subscription:
                 return subscription
             else:
