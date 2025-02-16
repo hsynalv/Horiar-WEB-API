f00a97367ca047d39905e9e73f72d4371b20fb72
beyazskorsky <hhsynalv@gmail.com>
2025-02-16 23:37:43 +0300

    support mail has been done

--- app/services/support_service.py
+++ app/services/support_service.py
@@ -1,5 +1,7 @@
 import logging
 
+from app.utils.automate_mail import send_support_reset_email
+
 support_logger = logging.getLogger("support")
 
 def log_support_request(name, message, payload):
@@ -12,3 +14,5 @@ def log_support_request(name, message, payload):
     # Log kaydı oluştur
     log_message = f"Name: {name} | E-mail: {email} | Message: {message}"
     support_logger.info(log_message)
+
+    send_support_reset_email(email, name, message)
--- app/utils/automate_mail.py
+++ app/utils/automate_mail.py
@@ -69,3 +69,26 @@ def send_password_reset_email(email, reset_link):
         return response.json()
     except requests.exceptions.RequestException as e:
         logging.error(f"Reset şifre email gönderme hatası: {e}") 
+
+def send_support_reset_email(email, username, message):
+    """
+    Kullanıcıya destek e-postası gönderir.
+    """
+    payload = {
+        "email": email,
+        "type": "support",
+        "data": {
+            "name": username,
+            "message": message
+        }
+    }
+
+    logging.info(f"payload: {payload}")
+
+    try:
+        response = requests.post("http://3.68.189.144:3000/send-email", json=payload)
+        response.raise_for_status()  # Hata durumunda istisna fırlatır
+        logging.info("Support email başarılı şekilde gönderildi!", response.json())
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Support email gönderme hatası: {e}") 
